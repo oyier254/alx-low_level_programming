f565aa6a22e6b06234b53a5e0e58a8db3a6b9219
eugeneoyier <eugeneoyier@outlook.com>
2023-06-19 18:39:41 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Description: print all single digit in base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		printf("%d", d);
+
+	printf("\n");
+	return (0);
+}
