f982179822c9451278a740eea588197e28ad1c54
eugeneoyier <eugeneoyier@outlook.com>
2023-09-22 02:07:24 -0700

    argc, argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int main (int argc, char *argv[]);
+
+#endif
