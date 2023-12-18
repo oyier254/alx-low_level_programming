820c3d2a8beb12f88aeadfc5f20f6ead010af54d
eugeneoyier <eugeneoyier@outlook.com>
2023-12-19 00:13:32 +0300

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,51 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <stdlib.h>
+
 
 /**
- * read_textfile - This reads a text file and prints it to the POSIX stdo
+ * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
  * @letters: number of letters it should read and print
- * return: actual number of letters it could read and promt
- * */
+ * Return: number of letters it could read and print, 0 if filename is NULL
+ * or if write fails or does not write the expected amount of bytes
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_d;
-    ssize_t lenr, lenw;
-    char *buffer;
+	int fd;
+	char *buf;
+	ssize_t read_bytes, write_bytes;
 
-    if (filename == NULL)
-    return(0);
-    file_d = open(filename, O_RDONLY);
-    if (file_d == -1)
-    return (0);
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(file_d);
-        return (0);
-    }
-    lenr = read(file_d, buffer, letters);
-    close(file_d);
-    if (lenr == -1)
-    {
-        free(buffer);
-        if (lenr !=lenw)
-        return(0);
-        return (lenw);
-    }
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	read_bytes = read(fd, buf, letters);
+	if (read_bytes == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	write_bytes = write(STDOUT_FILENO, buf, read_bytes);
+	if (write_bytes == -1 || write_bytes != read_bytes)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+	return (write_bytes);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,18 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUF_SIZE 1024
+#define ERR_MSG_LEN 50
+#define MAGIC_LEN 4
+#define ELF_HEADER_SIZE 52
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fontl.h>
+#include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-#int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
-
