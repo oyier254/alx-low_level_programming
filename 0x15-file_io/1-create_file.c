a56936cd06b60f1d95d8d59f34cd5a6427119723
eugeneoyier <eugeneoyier@outlook.com>
2023-12-19 00:19:03 +0300

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: name of the file
- * @text_content: NULL terminated string to add at the end of the file
- * Return: 1 on success and -1 on failure
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int fd, len, write_bytes;
+	int fd, i, write_count;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -24,16 +25,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	for (len = 0; text_content[len]; len++)
+	for (i = 0; text_content[i] != '\0'; i++)
 		;
 
-	write_bytes = write(fd, text_content, len);
-	if (write_bytes == -1)
-	{
-		close(fd);
+	write_count = write(fd, text_content, i);
+	if (write_count == -1)
 		return (-1);
-	}
 
 	close(fd);
+
 	return (1);
 }
