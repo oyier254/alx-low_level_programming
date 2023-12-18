4df4a2b09bf95b8497ab61ee7154ca79333f1f91
eugeneoyier <eugeneoyier@outlook.com>
2023-12-19 00:16:29 +0300

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,23 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
 #define BUF_SIZE 1024
 #define ERR_MSG_LEN 50
 #define MAGIC_LEN 4
 #define ELF_HEADER_SIZE 52
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int remove_file(const char *filename);
 
 #endif
