bbf05a60dd20a7f237f689a1799c05db93b71190
eugeneoyier <eugeneoyier@outlook.com>
2023-12-19 00:15:19 +0300

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,43 @@
 #include "main.h"
 
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
+ * read_textfile - reads a text file and prints it to
+ * the POSIX standard output
+ * @filename: file to read
  * @letters: number of letters it should read and print
- * Return: number of letters it could read and print, 0 if filename is NULL
- * or if write fails or does not write the expected amount of bytes
+ *
+ * Return: actual number of letters it could read and print
+ * 0 if filename is NULL or if write fails or does not write the expected
+ * amountof bytes
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *buf;
-	ssize_t read_bytes, write_bytes;
+	ssize_t read_count, write_count;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		free(buf);
 		return (0);
-	}
 
-	read_bytes = read(fd, buf, letters);
-	if (read_bytes == -1)
-	{
-		free(buf);
-		close(fd);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
-	}
 
-	write_bytes = write(STDOUT_FILENO, buf, read_bytes);
-	if (write_bytes == -1 || write_bytes != read_bytes)
-	{
-		free(buf);
-		close(fd);
+	read_count = read(fd, buffer, letters);
+	if (read_count == -1)
 		return (0);
-	}
 
-	free(buf);
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	if (write_count == -1 || write_count != read_count)
+		return (0);
+
 	close(fd);
-	return (write_bytes);
+	free(buffer);
+
+	return (write_count);
 }
